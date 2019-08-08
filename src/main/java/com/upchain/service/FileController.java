3b2a920f3920b00626ae600ebdd6291be926341b
Sridharan Murugadass <sridharanbe.ece@gmail.com>
2019-08-08 12:45:08 +0100

    Update FileController.java

--- src/main/java/com/upchain/service/FileController.java
+++ src/main/java/com/upchain/service/FileController.java
@@ -43,12 +43,4 @@ public class FileController {
 		return "File Deleted Successfully";
 	}
 
-	private File convertMultiPartToFile(MultipartFile file) throws IOException {
-		File convFile = new File(file.getOriginalFilename());
-		FileOutputStream fos = new FileOutputStream(convFile);
-		fos.write(file.getBytes());
-		fos.close();
-		return convFile;
-	}
-
 }
